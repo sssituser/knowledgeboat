3f10e4aa91e1364f3de20a39ac7110b9f5d24e8d
NavyaSri <navyasricseb71@gmail.com>
2025-12-09 17:20:27 +0530

    Knowledgeboat project is added

---
+++ Knowledgeboat/Knowledgeboat/src/main/java/com/example/knowledgeboat/KnowledgeboatApplication.java
@@ -0,0 +1,13 @@
+package com.example.knowledgeboat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeboatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowledgeboatApplication.class, args);
+    }
+
+}
---
+++ Knowledgeboat/Knowledgeboat/src/test/java/com/example/knowledgeboat/KnowledgeboatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.knowledgeboat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeboatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
